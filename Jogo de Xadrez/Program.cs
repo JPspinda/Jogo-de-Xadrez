1c95e615ee8ffc523e7408d2640309804e3ac286
JPspinda <jppaulinospinda@gmail.com>
2025-08-30 10:28:08 -0300

    Criando a classe posição

--- Jogo de Xadrez/Program.cs
+++ Jogo de Xadrez/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace JogoDeXadrez
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
         }
     }
 }
