c2539e9d733584e1f8fe98cc469d542dc54c08b1
JPspinda <jppaulinospinda@gmail.com>
2025-09-01 11:35:07 -0300

    Criando a classe posição xadrez

--- Jogo de Xadrez/Program.cs
+++ Jogo de Xadrez/Program.cs
@@ -2,6 +2,7 @@
 using Jogo_de_Xadrez;
 using tabuleiro;
 using xadrez;
+using Xadrez;
 
 namespace JogoDeXadrez
 {
@@ -9,20 +10,11 @@ namespace JogoDeXadrez
     {
         static void Main(string[] args) 
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 9));
+            Console.WriteLine(pos);
 
-                Tela.ImprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos.ToPosicao());
         }
     }
 }
---
+++ Jogo de Xadrez/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿
+using tabuleiro;
+namespace Xadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao ToPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a'); // cada caracter possui um código para representar sua letra, e quando colocamos um caraceter menos 'a' estamos praticamente convertendo eles para numeros
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
